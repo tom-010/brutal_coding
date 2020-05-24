e4cbc6911375c8bb5d6fe032c7c05c1980bb358d
Thomas Deniffel <t.deniffel@canary-works.com>
2020-05-24 16:04:05 +0200

    moved parser

--- test_parser.py
+++ test_parser.py
@@ -1,36 +1,5 @@
 import unittest
-from collections import namedtuple
-
-
-Line = namedtuple('Line', ['file', 'line'])
-
-class Parser():
-    
-    def parse(self, s):
-        if not s:
-            return []
-        
-        lines = []
-        for line in s.split('\n'):
-            parts = [x for x in line.split('  ') if x]
-            if len(parts) == 5 and parts[4] != ' Missing':
-                for line_no in self.parse_lines_part(parts[4]):
-                    lines.append(Line(parts[0], line_no))
-
-
-        return lines
-
-    def parse_lines_part(self, s):
-        res = []
-        for p in s.split(', '):
-            if '-' in p:
-                [fromRange, toRange] = p.split('-')
-                for i in range(int(fromRange), int(toRange)+1):
-                    res.append(i)
-            else: 
-                res.append(int(p))
-        return res
-
+from parser import Parser, Line
 
 
 class TestParser(unittest.TestCase):
